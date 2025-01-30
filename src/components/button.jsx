2069436da63114bfd3b1d9178f6edcaf05d949ab
Shivanshu <shivanshu192004@gmail.com>
2025-01-30 18:06:15 +0530

    Changed navbar button size

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -7,7 +7,7 @@ const themeCSS={
 
 const variants={
     "small":"text-sm px-4 py-1 sm:text-base rounded-md",
-    "medium":"text-sm px-4 py-1 sm:text-lg rounded-md",
+    "medium":"text-xs px-4 py-1 sm:text-sm md:text-base rounded-md",
     "large":"text-base sm:text-lg px-4 py-2 rounded-lg"
 }
 
